b166ef56041386d9b25740e8a5b3722a229a4c80
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-10-26 11:45:47 -0300

    Database config

---
+++ backend/src/database/config/database.config.ts
@@ -0,0 +1,12 @@
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const sequelize = new Sequelize({
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	host: process.env.DB_HOST,
+	port: 5432,
+});
