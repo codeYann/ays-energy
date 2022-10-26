45e218e26e79f0066f3acf88230416218115a23d
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-10-26 06:05:38 -0300

    Index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import express from "express";
+import dotenv from "dotenv";
+import Routes from "./routes";
+
+const server = express();
+dotenv.config();
+
+server.use(express.json());
+server.use(Routes);
+server.listen(process.env.PORT, () => {
+	console.log(`Server running on localhost:${process.env.PORT || "3506"}`);
+});
