9e8463e1091454d8df8b9213218f97c71f03853a
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-10-30 08:15:05 -0300

    Authors controller

--- backend/src/controllers/AuthorsController.ts
+++ backend/src/controllers/AuthorsController.ts
@@ -1,32 +1,85 @@
 import { Request, Response } from "express";
-import AuthorModel from "../models/author";
+import AuthorsModel from "../models/authors";
+import { ServerResponseError } from "../utils/internal-error";
 
-export default class AuthorsController {
-	public async index(_: Request, response: Response): Promise<void> {
+type AuthorsDecoderType = {
+	id?: number;
+	name: string;
+	email: string;
+};
+
+export class AuthorsController {
+	public async index(request: Request, response: Response): Promise<void> {
 		try {
-			const authorsList = await AuthorModel.findAll();
+			const authorsList = await AuthorsModel.findAll();
 			response.status(200).send(authorsList);
 		} catch (error) {
-			response
-				.status(204)
-				.send({ message: "There are no authors available yet" });
+			throw new ServerResponseError(
+				`Unable to export a list of authors. ${error}`
+			);
 		}
 	}
 
-	public async store(request: Request, response: Response): Promise<void> {
+	public async store(
+		request: Request,
+		response: Response
+	): Promise<AuthorsModel> {
 		try {
-			const { name, email }: { name: string; email: string } = request.body
+			const { name, email }: AuthorsDecoderType = request.body;
 
-			const newAuthor = await AuthorModel.create({
+			const newAuthor = await AuthorsModel.create({
 				name,
 				email,
 			});
 
-			response.status(200).send(newAuthor);
+			response.status(201).send(newAuthor);
+			return newAuthor;
 		} catch (error) {
-			response
-				.status(404)
-				.send({ message: "It was not possible to create a new author" });
+			throw new ServerResponseError(`Unable to create an author. ${error}`);
+		}
+	}
+
+	public async update(
+		request: Request,
+		response: Response
+	): Promise<[affectedCount: number]> {
+		try {
+			const id = request.params["id"];
+
+			const { email: newEmail }: { email: string } = request.body;
+
+			const updatedAuthor = await AuthorsModel.update(
+				{ email: newEmail },
+				{
+					where: {
+						id: parseInt(id),
+					},
+				}
+			);
+
+			response.status(200).send(updatedAuthor);
+			return updatedAuthor;
+		} catch (error) {
+			throw new ServerResponseError(`Unable to update an author. ${error}`);
+		}
+	}
+
+	public async delete(request: Request, response: Response): Promise<number> {
+		try {
+			const { id }: { id: string } = request.body;
+
+			const removedAuthor = await AuthorsModel.destroy({
+				where: {
+					id: parseInt(id),
+				},
+			});
+
+			console.log(request.body);
+
+			response.status(200).send({ id });
+			return removedAuthor;
+		} catch (error) {
+			throw new ServerResponseError(`Unable to delete an author. ${error}`);
 		}
 	}
 }
