574c07926034de24a890ec02468591e80eeb39e1
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-10-30 08:15:39 -0300

    Define a class to server's response error

---
+++ backend/src/utils/internal-error.ts
@@ -0,0 +1,18 @@
+class InternalError extends Error {
+	constructor(
+		public message: string,
+		protected code: number = 500,
+		protected description?: string
+	) {
+		super(message);
+		this.name = this.constructor.name;
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+export class ServerResponseError extends InternalError {
+	constructor(message: string) {
+		const internalMessage = "Unexpected error when trying to comunicate to Api";
+		super(`${internalMessage}. ${message}`);
+	}
+}
