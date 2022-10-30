f4b8f96d5be4a38045c86d1e682c070eb64ace08
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-10-30 08:15:55 -0300

    Authors model

---
+++ backend/src/models/authors.ts
@@ -0,0 +1,39 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../database/config";
+
+class AuthorsModels extends Model {}
+
+AuthorsModels.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+			allowNull: false,
+		},
+		name: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		createdAt: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+		updatedAt: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		tableName: "authors",
+		timestamps: true,
+	}
+);
+
+export default AuthorsModels;
