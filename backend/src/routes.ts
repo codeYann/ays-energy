d046a4d7997ea79bed3dde5ca4e85f88e7d8ad6b
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-10-30 08:04:37 -0300

    create a function to export all routes of an author and a post

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,29 @@
 import { Router } from "express";
+import { AuthorsController } from "./controllers/AuthorsController";
+import { PostsController } from "./controllers/PostsController";
+
+const authorsController = new AuthorsController();
+const postsController = new PostsController();
 
 class Routes {
 	public router: Router;
 
 	constructor() {
 		this.router = Router();
+		this.authorsEndPoints();
+		this.postsEndPoints();
+	}
+
+	public authorsEndPoints(): void {
+		this.router.get("/authors", authorsController.index);
+		this.router.post("/authors", authorsController.store);
+		this.router.delete("/authors", authorsController.delete);
+		this.router.patch("/authors/:id", authorsController.update);
+	}
+
+	public postsEndPoints() {
+		this.router.get("/post", postsController.index);
+		this.router.post("/post", postsController.store);
 	}
 }
 
