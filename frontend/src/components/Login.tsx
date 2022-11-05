e3e6a6adf063d19a69edc9a61b9702c791295e8c
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-11-05 10:32:13 -0300

    Login component

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export default function Login() {
+	return (
+		<div>
+			<Button variant='outlined'> what up dude </Button>
+		</div>
+	);
+}
