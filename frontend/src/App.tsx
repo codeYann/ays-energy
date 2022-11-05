c42431d0428e6066aadde8cfb551c031c5d2f161
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-11-05 10:34:14 -0300

    Adding router in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {CssBaseline} from '@mui/material';
+import Login from './components/Login';
+
+const router = createBrowserRouter([
+	{
+		path: '/login',
+		element: <Login/>,
+	},
+]);
 
 function App() {
-	return <div className='app'>ola mundo</div>;
+	return (
+		<div className='App'>
+			<CssBaseline/>
+			<RouterProvider router={router}/>
+		</div>
+	);
 }
 
 export default App;
