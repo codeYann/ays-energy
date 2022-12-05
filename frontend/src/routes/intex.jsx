2053e9d9876be7d15fff0ee3d5ec9b6103be8fb8
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-12-05 08:30:01 -0300

    Index.jsx

--- frontend/src/routes/intex.jsx
+++ frontend/src/routes/intex.jsx
@@ -1,18 +1,17 @@
-import { Route, Routes,BrowserRouter } from "react-router-dom";
-import Home from "../pages/home";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from '../pages/home';
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
-export default function ApplicationRoutes(){
-
-    return(
-        <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    );
-
+export default function ApplicationRoutes() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
