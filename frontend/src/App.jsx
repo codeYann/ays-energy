012d8a4ce47e9745748169407188bdaf891910f2
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-12-05 08:27:03 -0300

    Transformando app.jsx como rota principal e adicionando a lógica das rotas, nesse arquivo basta adicionar as páginas e colocar no vetor de rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css';
-import ApplicationRoutes from './routes/intex';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+import Home from './pages/home';
+import DashBoard from './pages/dashboard';
+import LoginDashBoard from './components/login-dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <LoginDashBoard />,
+  },
+  {
+    path: '/dashboard',
+    element: <DashBoard />,
+  },
+]);
 
 function App() {
-  return(
-    <>
-        <ApplicationRoutes />
-    </>
+  return (
+    <div>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </div>
   );
-  
 }
 
 export default App;
